217046bded564f3c39c2854b8ac209468df0d5d5
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-11-03 11:20:50 -0800

    Light refactor and cleanup

--- src/mininim/web.nim
+++ src/mininim/web.nim
@@ -3,9 +3,7 @@ import
     mininim,
     mininim/dic,
     mininim/cli,
-    std/streams,
-    std/strutils,
-    std/os
+    std/streams
 
 from std/httpcore import
     HttpCode,
@@ -37,14 +35,44 @@ type
         headers*: HttpHeaders
 
 begin Request:
+    discard
+
+begin Request:
     method get*(name: string, default: string = ""): string {. base .} =
         let
             value = this.pathParams.getOrDefault(name, default)
 
         result = if value != "": value else: default
 
+begin Middleware:
+    discard
+
+#[
+
+    TODO: This currently needs to before Handler, otherwise, the default Middleware call is not
+    defined and doesn't get imported for Handler's Middlware() facet.
+]#
+shape Middleware: @[
+    Hook(
+        call: proc(server: HttpServer, request: Request, pos: int): Response {. closure .}=
+            let
+                head = this.app.get(shape)
+                next = proc(request: Request): Response =
+                    if pos > server.middleware.high:
+                        result = Response(status: HttpCode(404))
+                    else:
+                        result = MiddlewareHook.value(server.middleware[pos].call)(
+                            server,
+                            request,
+                            pos + 1
+                        )
+
+            result = head.handle(request, next)
+    )
+]
+
 begin Handler:
-    method handle(request: Request, next: MiddlewareNext): Response {. base, gcsafe .} =
+    method handle*(request: Request, next: MiddlewareNext): Response {. base .} =
         result        = next(request)
         result.status = HttpCode(200)
         result.stream = newStringStream("Hello Mininim!")
@@ -52,32 +80,6 @@ begin Handler:
 shape Handler: @[
     Middleware(
         name: "default"
-    )
-]
-
-shape Middleware: @[
-    Hook(
-        call: proc(server: HttpServer, request: Request, pos: int): Response {. closure .}=
-            let
-                current = this.app.get(shape)
-
-            if pos > server.middleware.high:
-                result = current.handle(
-                    request,
-                    proc(request: Request): Response =
-                        result = Response(status: HttpCode(404))
-                )
-
-            else:
-                result = current.handle(
-                    request,
-                    proc(request: Request): Response =
-                        result = MiddlewareHook.value(server.middleware[pos].call)(
-                            server,
-                            request,
-                            pos + 1
-                        )
-                )
     )
 ]
 
@@ -92,7 +94,9 @@ begin HttpServer:
                         response = MiddlewareHook.value(this.middleware[0].call)(
                             this,
                             request,
-                            1
+                            1 # There is always at least one middleware, so we start this at 1
+                              # to ensure that the next callback generated in the Middleware Hook
+                              # will return a 404 appropriately.
                         )
 
                     request.respond(
@@ -118,19 +122,22 @@ begin HttpServer:
         let
             middlewares = app.config.findAll(Middleware)
 
-        if middlewares.len == 0:
+        if middlewares.len == 1:
             let
-                default = app.config.findAll(Middleware, (scope: Handler.typeID))
+                default = app.config.findOne(Middleware, (scope: Handler.type))
 
             result.middleware.add(default)
 
+            when defined(debug):
+                echo fmt "message[{align($default.class, 3, '0')}] registered middleware '{default.name}'"
+
         else:
             for middleware in middlewares:
-                if middleware.scope != Handler.typeID:
+                if middleware.scope != Handler:
                     result.middleware.add(middleware)
 
-                when defined(debug):
-                    echo fmt "message[{align($middleware.class, 3, '0')}] registered middleware {middleware.name}"
+                    when defined(debug):
+                        echo fmt "message[{align($middleware.class, 3, '0')}] registered middleware '{middleware.name}'"
 
     method execute(console: Console): int {. base .} =
         result = this.run()
